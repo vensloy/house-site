e08cae3cffb31afe8ba20cb3faf599bdd4bf473e
Anton <komogrcevanton10@gmail.com>
2025-10-07 21:21:32 +0800

    style: maked the header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import './css/header.css'
 import './css/reset.css'
 
+import icon from './assets/img/header/icon.svg'
+import basket from './assets/img/header/basket.svg'
+
 const App = () => {
   return (
     <header>
-      
+      <a href="#" className="icon">
+        <img src={icon} alt="" />
+        <p>Ink. House</p>
+      </a>
+
+      <nav>
+        <a href="#">Репродукции</a>
+        <a href="#">Новинки</a>
+        <a href="#">О нас</a>
+        <img src={basket} alt="" />
+      </nav>
     </header>
   )
 }
