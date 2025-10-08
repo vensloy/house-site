617850bdc655a3111472777029b539885a473a0c
Anton <komogrcevanton10@gmail.com>
2025-10-08 19:18:58 +0800

    style: add all folders for the main section

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import './css/header.css'
 import './css/reset.css'
+import './css/main.css'
 
 import icon from './assets/img/header/icon.svg'
 import basket from './assets/img/header/basket.svg'
 
 const App = () => {
   return (
-    <header>
-      <a href="#" className="icon">
-        <img src={icon} alt="" />
-        <p>Ink. House</p>
-      </a>
+    <>
+      <header>
+        <a href="#" className="icon">
+          <img src={icon} alt="" />
+          <p>Ink. House</p>
+        </a>
 
-      <nav>
-        <a href="#">Репродукции</a>
-        <a href="#">Новинки</a>
-        <a href="#">О нас</a>
-        <img src={basket} alt="" />
-      </nav>
-    </header>
+        <nav>
+          <a href="#">Репродукции</a>
+          <a href="#">Новинки</a>
+          <a href="#">О нас</a>
+          <img src={basket} alt="" />
+        </nav>
+      </header>
+
+      <main>
+
+      </main>
+    </>
   )
 }
 
