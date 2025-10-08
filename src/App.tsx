67d34d20630cd0eb46df5e0e8ad5c780625c8e9e
Anton <komogrcevanton10@gmail.com>
2025-10-08 19:59:25 +0800

    style: maked the hero section

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './css/main.css'
 
 import icon from './assets/img/header/icon.svg'
 import basket from './assets/img/header/basket.svg'
+import birdHero from './assets/img/main/hero/bird.png'
 
 const App = () => {
   return (
@@ -24,7 +25,13 @@ const App = () => {
 
       <main>
         <section className='hero'>
-          
+          <img className='bird-hero-img' src={birdHero} alt="" />
+
+          <section className='right-hero'>
+            <h2 className='heading-hero'>Реплики картин от <div className='heading-name'>Ink.House</div></h2>
+            <p className='right-hero-text'>Высокое качество отрисовки на плотной бумаге или льняном холсте. <br /> Редкие произведения, доступные цены.</p>
+            <button className='products-button'>Продукция</button>
+          </section>
         </section>
       </main>
     </>
